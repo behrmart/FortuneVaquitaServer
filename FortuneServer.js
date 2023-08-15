f7e450c892401e05a8645bae745e18b36860f39d
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-08-15 11:53:52 -0600

    added fortune in JSON format

--- FortuneServer.js
+++ FortuneServer.js
@@ -3,6 +3,7 @@
 
 //URL https://Server:3333/fortune Only fortune text
 //URL https://Server:3333/vaquita Vaquita format
+//URL https://Server:3333/fortunejson Fortune JSON format
 
 const express = require('express');
 const { exec } = require('child_process');
@@ -37,7 +38,22 @@ app.get('/vaquita', (req, res) => {
     });
   });
 
+  app.get('/fortunejson', (req, res) => {
+    const command = 'fortune'; // Linux command
   
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        res.status(500).send(`Error executing command: ${error.message}`); // If generic error produced 500 error
+      } else if (stderr) {
+        res.status(500).send(`Command stderr: ${stderr}`); // If stderr message produced 500 error
+      } else {
+        res.json({ fortune: `${stdout}`}); //Send command output in JSON format
+      }
+    });
+  });
+
+
+
   app.listen(port, () => {
     console.log(`Fortune Vaquita Server listening on port ${port}`);
   });
