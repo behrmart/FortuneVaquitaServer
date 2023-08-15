5faa24d22e8a9b12d70611d4d21ec2cd1dbdd3d5
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-08-15 11:40:33 -0600

    added fortune and vaquita URLs

--- FortuneServer.js
+++ FortuneServer.js
@@ -1,27 +1,42 @@
 // Fortune|cowsay vaquita Server using Node.js and Express 
 // Bernardo F Martinez Meave
 
-//URL https://Server:3333/fortune
+//URL https://Server:3333/fortune Only fortune text
+//URL https://Server:3333/vaquita Vaquita format
 
 const express = require('express');
 const { exec } = require('child_process');
 const app = express();
-const port = 3333;
+const port = 3333; // Listening TCP Port 
 
-app.get('/fortune', (req, res) => {
+app.get('/vaquita', (req, res) => {
     const command = 'fortune | cowsay -f apt'; // Linux command
   
     exec(command, (error, stdout, stderr) => {
       if (error) {
-        res.status(500).send(`Error executing command: ${error.message}`);
+        res.status(500).send(`Error executing command: ${error.message}`); // If generic error produced 500 error
       } else if (stderr) {
-        res.status(500).send(`Command stderr: ${stderr}`);
+        res.status(500).send(`Command stderr: ${stderr}`); // If stderr message produced 500 error
       } else {
-        res.send(`<pre>${stdout}</pre>`);
+        res.send(`<pre>${stdout}</pre>`); //Send command output to stdout
       }
     });
   });
   
+  app.get('/fortune', (req, res) => {
+    const command = 'fortune'; // Linux command
+  
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        res.status(500).send(`Error executing command: ${error.message}`); // If generic error produced 500 error
+      } else if (stderr) {
+        res.status(500).send(`Command stderr: ${stderr}`); // If stderr message produced 500 error
+      } else {
+        res.send(`<pre>${stdout}</pre>`); //Send command output to stdout
+      }
+    });
+  });
+
   
   app.listen(port, () => {
     console.log(`Fortune Vaquita Server listening on port ${port}`);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FortuneVaquitaServer
+
+# Fortune|cowsay vaquita Server using Node.js and Express 
+# by Bernardo F Martinez Meave
+# Use URL https://Server:3333/fortune
+
+Need to have node.js express fortune and cowsay installed in a linux server
