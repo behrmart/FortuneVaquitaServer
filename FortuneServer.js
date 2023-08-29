f45db79bd101c62b53e7e90503c02bd925383106
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2023-08-29 13:06:23 -0600

    added delay parameter

--- FortuneServer.js
+++ FortuneServer.js
@@ -5,6 +5,18 @@
 //URL https://Server:3333/vaquita Vaquita format
 //URL https://Server:3333/fortunejson Fortune JSON format
 
+
+// Access command-line arguments
+const arguments = process.argv.slice(2);
+let delay = 0;
+// Check if arguments were provided
+if (arguments.length === 0) {
+  console.log("Fetch Delay is 0s / Usage: node FortuneServer.js delayInmilliseconds");
+} else {
+  console.log("Fetch Delay: ", arguments[0] + " milliseconds");
+  delay = arguments[0];
+}
+
 const express = require('express');
 const { exec } = require('child_process');
 const app = express();
@@ -55,34 +67,38 @@ app.get(vaquitaURL, (req, res) => {
 
   //JSON format
   app.get(jsonURL, (req, res) => {
-    const command = 'fortune -c'; // Linux command
-  
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        res.status(500).send(`Error executing command: ${error.message}`); // If generic error produced 500 error
-      } else if (stderr) {
-        res.status(500).send(`Command stderr: ${stderr}`); // If stderr message produced 500 error
-      } else {
-        const stdoutput = stdout;
-        //console.log ('stdout: ' + stdout)
-        const fortuneType = stdoutput.match(/\((.*?)\)/g);
-        //console.log('TYPE: ' + fortuneType); // Parse fortune type between ()
-        const fortuneText = stdoutput.match(/(?<=%)([\s\S]*)/); // Parse text after type ()
-        
-        var obj = new Object();
-        obj.fortunetype = fortuneType;
-        obj.fortunetext = fortuneText;
-        var jsonString = JSON.stringify(obj);
 
-        const timestamp = new Date().toLocaleString();
+    //const delay = 2000;  //Delay in milliseconds (2 sec)
 
-        console.log(timestamp + ' FORTUNE TEXT JSON: ' + jsonString);
+    setTimeout(() => {
+        const command = 'fortune -c'; // Linux command
+      
+        exec(command, (error, stdout, stderr) => {
+          if (error) {
+            res.status(500).send(`Error executing command: ${error.message}`); // If generic error produced 500 error
+          } else if (stderr) {
+            res.status(500).send(`Command stderr: ${stderr}`); // If stderr message produced 500 error
+          } else {
+            const stdoutput = stdout;
+            //console.log ('stdout: ' + stdout)
+            const fortuneType = stdoutput.match(/\((.*?)\)/g);
+            //console.log('TYPE: ' + fortuneType); // Parse fortune type between ()
+            const fortuneText = stdoutput.match(/(?<=%)([\s\S]*)/); // Parse text after type ()
+            
+            var obj = new Object();
+            obj.fortunetype = fortuneType;
+            obj.fortunetext = fortuneText;
+            var jsonString = JSON.stringify(obj);
 
-        res.send(jsonString); //Send command output in JSON format
-      }
-    });
+            const timestamp = new Date().toLocaleString();
+
+            console.log(timestamp + ' FORTUNE TEXT JSON: ' + jsonString);
+
+            res.send(jsonString); //Send command output in JSON format
+          }
+        });
+      }, delay);
   });
-
 
   app.listen(port, () => {
     console.log(`Fortune Vaquita Server listening on port ${port}`);
