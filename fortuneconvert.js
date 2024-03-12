6fb4eaf8c6dca41358e5423327bff3335c997c36
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2024-03-12 17:54:55 -0600

    Added JS program to convert Fortune files to JSON

---
+++ fortuneconvert.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+
+// Function to convert fortunes to JSON
+function convertToJSON(data) {
+  const lines = data.split("\n");
+  const fortunes = [];
+  let currentFortune = "";
+
+  for (let line of lines) {
+    line = line.trim();
+
+    if (line.startsWith("%")) {
+      if (currentFortune.length > 0) {
+        fortunes.push(currentFortune.trim());
+        currentFortune = "";
+      }
+    } else if (line.length > 0) {
+      currentFortune += line + " ";
+    }
+  }
+
+  if (currentFortune.length > 0) {
+    fortunes.push(currentFortune.trim());
+  }
+
+  const jsonFortunes = fortunes.map((fortune, index) => {
+    return {
+      id: index + 1,
+      message: fortune,
+    };
+  });
+
+  return jsonFortunes;
+}
+
+// Read the file
+fs.readFile("./fortunes/fortunes", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+
+  // Convert to JSON
+  const jsonFortunes = convertToJSON(data);
+
+  // Print JSON
+  console.log(JSON.stringify(jsonFortunes, null, 2));
+
+  // You can also write the JSON to a file if needed
+  fs.writeFile(
+    "fortunes.json",
+    JSON.stringify(jsonFortunes, null, 2),
+    (err) => {
+      if (err) {
+        console.error("Error writing JSON:", err);
+        return;
+      }
+      console.log("Fortunes JSON file created!");
+    }
+  );
+});
