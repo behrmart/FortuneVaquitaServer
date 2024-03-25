a6ab6f690ddb2a515579bd070f83938058145af2
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2024-03-24 18:06:57 -0600

    Created random fortune controller and endpoint

--- controllers/fortunesController.js
+++ controllers/fortunesController.js
@@ -1,4 +1,4 @@
-// Avaya SDP customer log Backend
+// Fortune Backend Controller
 // by Bernardo F. Martinez Meave
 // Mongo DB Controller routines
 
@@ -7,23 +7,32 @@ const Fortune = require("../model/fortunesModel");
 const fs = require("fs");
 
 const getAllFortunes = asyncHandler(async (req, res) => {
+  console.log("Getting all fortunes");
   const fortunes = await Fortune.find();
   res.status(200).json(fortunes);
 });
 
 const getOneFortune = asyncHandler(async (req, res) => {
   Fortune.events.on("error", (err) => console.log(err.message));
-  console.log(req.params.id);
+  console.log("getting fortune No.:", req.params.id);
   const fortune = await Fortune.find({ fortune_id: req.params.id })
     .exec()
     .catch();
-  console.log(fortune);
+
   if (!fortune) {
     res.status(404);
     throw new Error("getFortune: Fortune ID not found");
   } else {
     res.status(200).json(fortune);
   }
+});
+
+const getRandomFortune = asyncHandler(async (req, res) => {
+  const count = await Fortune.countDocuments();
+  const random = Math.floor(Math.random() * count);
+  const fortune = await Fortune.findOne().skip(random);
+  console.log("Getting random fortune No.:", random);
+  res.status(200).json(fortune);
 });
 
 const createFortuneDB = asyncHandler(async (req, res) => {
@@ -38,5 +47,6 @@ const createFortuneDB = asyncHandler(async (req, res) => {
 module.exports = {
   getAllFortunes,
   getOneFortune,
+  getRandomFortune,
   createFortuneDB,
 };
--- routes/fortuneRoutes.js
+++ routes/fortuneRoutes.js
@@ -7,12 +7,14 @@ const router = express.Router();
 const {
   getAllFortunes,
   getOneFortune,
+  getRandomFortune,
   createFortuneDB,
 } = require("../controllers/fortunesController");
 // const { protect } = require("../middleware/authMiddleware");
 
 router.get("/getAllFortunes", getAllFortunes);
+router.get("/random", getRandomFortune);
 router.get("/:id", getOneFortune);
-router.get("/CreateFortuneDB", createFortuneDB);
+//router.get("/CreateFortuneDB", createFortuneDB);
 
 module.exports = router;
