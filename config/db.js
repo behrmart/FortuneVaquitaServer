5e90d0db331548aaaa1fd9ade55023bf24f299e8
Bernardo F. Martínez Meave <bfelipemm@gmail.com>
2024-04-01 15:45:03 -0600

    Added retries to contact MongoDB server

--- config/db.js
+++ config/db.js
@@ -11,18 +11,38 @@
 
 const mongoose = require("mongoose");
 
+const MAX_RETRIES = 2;
+let retryCount = 0;
+
+// Close the connection when the Node.js process is terminated
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log("MongoDB connection closed due to Node.js process termination");
+    process.exit(0);
+  });
+});
+
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(
-      "MongoDB connected on Host: ".yellow,
-      conn.connection.host.cyan,
-      " Port: ".yellow,
-      conn.connection.port
-    );
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
+  while (retryCount < MAX_RETRIES) {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(
+        "MongoDB connected on Host: ".yellow,
+        conn.connection.host.cyan,
+        " Port: ".yellow,
+        conn.connection.port
+      );
+      break; // If connected successfully, exit the loop
+    } catch (error) {
+      retryCount++;
+      console.error(`Connection attempt ${retryCount} failed:`, error.message);
+      if (retryCount === MAX_RETRIES) {
+        console.error("Max connection retries reached. Exiting...");
+        process.exit(1);
+      }
+      console.log("Retrying connection...");
+      await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds before retrying
+    }
   }
 };
 
